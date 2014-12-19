d47748fee0209bc491ae889d972aa5b483b133c6
Tom Whipple <twhipple@paypal.com>
2014-12-18 16:12:49 -0800

    better Version generation

---
+++ card.io/src/io/card/payment/Version.java
@@ -0,0 +1,11 @@
+package io.card.payment;
+
+/* Version.java
+ * See the file "LICENSE.md" for the full license governing this code.
+ */
+
+public class Version {
+    public static final String PRODUCT_VERSION = BuildConfig.PRODUCT_VERSION;
+    public static final String PRODUCT_NAME = BuildConfig.PRODUCT_NAME;
+    public static final String BUILD_TIME = BuildConfig.BUILD_TIME;
+}
