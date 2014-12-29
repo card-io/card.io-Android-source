c53e3bfda71ec350ab25cd0ee077fb15f8d973a1
Tom Whipple <twhipple@paypal.com>
2014-12-29 08:29:37 -0800

    Basic gradle build success

--- card.io/src/io/card/payment/Version.java
+++ card.io/src/io/card/payment/Version.java
@@ -4,8 +4,10 @@ package io.card.payment;
  * See the file "LICENSE.md" for the full license governing this code.
  */
 
+import io.card.development.BuildConfig;
+
 public class Version {
-    public static final String PRODUCT_VERSION = BuildConfig.PRODUCT_VERSION;
+    public static final String REVISION = BuildConfig.PRODUCT_VERSION;
     public static final String PRODUCT_NAME = BuildConfig.PRODUCT_NAME;
-    public static final String BUILD_TIME = BuildConfig.BUILD_TIME;
+    public static final String BUILD_DATE = BuildConfig.BUILD_TIME;
 }
