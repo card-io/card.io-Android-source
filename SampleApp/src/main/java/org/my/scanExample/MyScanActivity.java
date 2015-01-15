c2aa3a296a38481c03e5739d2763ed4164005696
Jeff Brateman <jbrateman@paypal.com>
2015-01-14 22:34:48 -0600

    optimize imports

--- SampleApp/src/main/java/org/my/scanExample/MyScanActivity.java
+++ SampleApp/src/main/java/org/my/scanExample/MyScanActivity.java
@@ -1,14 +1,14 @@
 package org.my.scanExample;
 
-import io.card.payment.CardIOActivity;
-import io.card.payment.CreditCard;
-import org.my.scanExample.R;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import io.card.payment.CardIOActivity;
+import io.card.payment.CreditCard;
 
 public class MyScanActivity extends Activity
 {
