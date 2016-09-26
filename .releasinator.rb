89ec7aec69bac38e362691ae771db80ecaaad320
Jeff Brateman <braebot@gmail.com>
2016-09-26 17:04:15 -0500

    enforce releasing with r12b NDK

--- .releasinator.rb
+++ .releasinator.rb
@@ -9,8 +9,8 @@ configatron.prerelease_checklist_items = [
 ]
 
 def validate_ndk_version()
-  # please use $ wget http://dl.google.com/android/repository/android-ndk-r11c-darwin-x86_64.zip to get old versoins of NDK 
-  expected_release = "Pkg.Desc = Android NDK Pkg.Revision = 11.2.2725575"
+  # please use $ wget http://dl.google.com/android/repository/android-ndk-r12b-darwin-x86_64.zip to get old versoins of NDK
+  expected_release = "Pkg.Desc = Android NDK Pkg.Revision = 12.1.2977051"
   actual_release = `cat local.properties | sed 's/\\\./_/g' > .properties.file; . .properties.file; x=\`cat $ndk_dir/source.properties\`; rm -rf .properties.file; echo $x`.strip
   if expected_release != actual_release
     Printer.fail("ndk version verification.  Expected:#{expected_release}.  Actual:#{actual_release}.")
