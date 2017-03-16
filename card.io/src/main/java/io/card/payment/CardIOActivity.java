1ff8f33741ab0c867bbc4803e492072cb8fc9143
lkorth <luke@lukekorth.com>
2017-03-16 10:42:25 -0400

    Fix manual entry Activity started twice when rejecting camera permission
    Fixes #128
    Fixes card-io/card.io-Android-SDK#150

--- card.io/src/main/java/io/card/payment/CardIOActivity.java
+++ card.io/src/main/java/io/card/payment/CardIOActivity.java
@@ -643,20 +643,15 @@ public final class CardIOActivity extends Activity {
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case PERMISSION_REQUEST_ID: {
-                waitingForPermission = false;
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                    showCameraScannerOverlay();
-
-                } else {
-                    manualEntryFallbackOrForced = true;
-                    // show manual entry - handled in onResume()
-                }
-                onResume();
+    public void onRequestPermissionsResult(int requestCode, String permissions[],
+                                           int[] grantResults) {
+        if (requestCode == PERMISSION_REQUEST_ID) {
+            waitingForPermission = false;
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                showCameraScannerOverlay();
+            } else {
+                // show manual entry - handled in onResume()
+                manualEntryFallbackOrForced = true;
             }
         }
     }
